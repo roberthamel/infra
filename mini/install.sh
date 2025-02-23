25baa0b55af207c9ee744547188d5c5ef5e455dd
Robert Hamel <rbthml@gmail.com>
2025-02-22 22:12:14 -0500

    Refactor docker and rnas configurations by removing obsolete files and adding new service definitions

--- mini/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eEuo pipefail
-
-if ! command -v make &> /dev/null; then
-  echo "make could not be found. Please install make and try again."
-  exit 1
-fi
-
-make up
--- rnas/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -eEuo pipefail
-
-echo "No install exists for rnas... exiting"
-exit 1
